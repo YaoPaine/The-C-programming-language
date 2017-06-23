fbef0a66669b2c99e1b4d82e23138404ddc00a5d
YaoPaine <yaohe1991@gmail.com>
2017-06-23 17:57:48 +0800

    modify

--- chapter_2_3.c
+++ chapter_2_3.c
@@ -2,8 +2,8 @@
 //  chapter_2.c
 //  Demo
 //
-//  Created by 何瑶 on 2017/6/23.
-//  Copyright © 2017年 何瑶. All rights reserved.
+//  Created by YaoPaine on 2017/6/23.
+//  Copyright © 2017年 YaoPaine. All rights reserved.
 //
 
 #include <stdio.h>
